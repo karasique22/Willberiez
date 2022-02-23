9d31d8e3c1488b2bd0d4b20d99d721009eb653bc
oguzok <timofeidudin@mail.ru>
2022-02-24 00:00:39 +0300

    first commit

---
+++ js/cart.js
@@ -0,0 +1,11 @@
+const cartBtn = document.querySelector('.button-cart');
+const cart = document.getElementById('modal-cart');
+const closeBtn = document.querySelector('.modal-close');
+
+cartBtn.addEventListener('click', () => {
+    cart.style.display = 'flex';
+})
+
+closeBtn.addEventListener('click', () => {
+    cart.style.display = '';
+})
